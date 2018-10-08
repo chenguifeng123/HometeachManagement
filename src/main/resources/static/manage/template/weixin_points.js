f645796b3cdd3f4ae1201515782b7c6fdef1d60d
chenguifeng <22255946@qq.com>
2018-10-08 15:01:02 +0800

    submit new change

--- src/main/resources/static/manage/template/weixin_points.js
+++ src/main/resources/static/manage/template/weixin_points.js
@@ -26,7 +26,8 @@
                     initData : [{value:0, desc:'Fingerprints 1A'},{value:1, desc:'Fingerprints 1B'},{value:2, desc:'Fingerprints 2A'},
                         {value:3, desc:'Fingerprints 2B'},{value:4, desc:'Fingerprints 3A'},{value:5, desc:'Fingerprints 3B'},
                         {value:6, desc:'Phonics'},{value:7, desc:'Nursery Rhyme'},{value:8, desc:'First Friends 1'},{value:9, desc:'First Friends 2'},
-						{value:10, desc:'First Friends 3'}],
+						{value:10, desc:'First Friends 3'},{value:11, desc:'剑桥少儿英语1上'},{value:12, desc:'剑桥少儿英语1下'},
+						{value:13, desc:'剑桥少儿英语2上'},{value:14, desc:'剑桥少儿英语2下'},{value:15, desc:'剑桥少儿英语3上'},{value:16, desc:'剑桥少儿英语3下'}],
                     refs:{},
                     value : 0
                 },
